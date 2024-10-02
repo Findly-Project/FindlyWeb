c7558ae6fc64cda5e54c1af8622fefcde4ec58b2
kolo <sabatt148@gmail.com>
2024-10-02 20:41:27 +0300

    adding logic to get data on onliner

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-import quart
+from quart import Quart, request, render_template
+
+app = Quart(__name__)
+
+
+@app.route('/')
+async def main_view():
+
+    return await render_template('base.html')
+
+
+if __name__ == '__main__':
+
+    app.run()
