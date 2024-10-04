b3893819ba342e564244fbf0e107dc6abdb4af73
kolo <sabatt148@gmail.com>
2024-10-05 01:34:55 +0300

    add kufar logic

--- get_pars-data_func/get_21vek_data.py
+++ get_pars-data_func/get_21vek_data.py
@@ -1,29 +1,36 @@
-from typing import List, Tuple, Dict
+from typing import List, Dict
 import requests
 from requests import Response
-import json
+from bs4 import BeautifulSoup
+import re
 
 
-def get_21vek_data(query: str) -> Tuple[Tuple[str]]:
-    query: str = query.strip().replace(' ', '+')
-    url: str = f'https://catalog.onliner.by/sdapi/catalog.api/search/products?query={query}'
+def get_21vek_data(query: str) -> List[Dict]:
+    query: str = query.strip()
+    url: str = f'https://www.21vek.by/search/?sa=&term={query}'
 
     data: Response = requests.get(url)
-    data: Dict = json.loads(data.text)
+    soup: BeautifulSoup = BeautifulSoup(data.text, 'html.parser')
+    data: List = soup.find_all('li', class_='g-box_lseparator')
+    result_list: List[Dict[str | float], ...] = []
+    for i in data:
+        try:
+            price = i.find('span', class_='j-item-data').text
+            price = float(re.sub(r'[, ]', '', price.rstrip('0')))
 
-    result_list: List[Tuple[str], ...] = []
-
-    for i in data['products']:
-        item: Tuple[str, ...] = (i['html_url'],
-                                 i['full_name'],
-                                 i['description'],
-                                 i['prices']['price_min']['amount'])
+            item: Dict[str | float, ...]
+            item = {'link': i.find('a', class_='j-ga_track')['href'],
+                    'name': i.find('span', class_='result__name').text,
+                    'image': i.find('span', class_='result__img__inner').img['src'],
+                    'price': price}
+        except (TypeError, AttributeError) as e:
+            continue
 
         result_list.append(item)
 
-    return tuple(result_list)
+    return result_list
 
 
-res = get_onliner_data('принтер')
+res = get_21vek_data('samsung s24')
 for c in res:
     print(c)
