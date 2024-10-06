859eda74b613cec8f0f22130d24270c168cb2aef
koloideal <sabatt148@gmail.com>
2024-10-06 14:23:57 +0300

    some

---
+++ get_products_data/middlewares/finding_best_result/regular_expression.py
@@ -0,0 +1,16 @@
+import re
+from typing import List
+
+
+def regular_expression(query: str, candidates: List[str]) -> List[str]:
+
+    clear_candidates: List[str] = []
+
+    for candidate in candidates:
+
+        is_best_result: bool = bool(re.match(fr'.*{query}.*', candidate, re.IGNORECASE))
+
+        if is_best_result:
+            clear_candidates.append(candidate)
+
+    return clear_candidates
--- get_products_data/middlewares/finding_best_result/vector_representations_of_words.py
+++ get_products_data/middlewares/finding_best_result/vector_representations_of_words.py
@@ -10,9 +10,11 @@ def vector_representations_of_words(query: str, candidates: list):
     candidate_docs = [nlp(candidate) for candidate in candidates]
 
     similarities = [query_doc.similarity(candidate_doc) for candidate_doc in candidate_docs]
+    print(similarities)
 
     best_match_idx = similarities.index(max(similarities))
 
     return candidates[best_match_idx]
 
 
+print(vector_representations_of_words('iphone 16', ['iphone 16 pro', 'iphone 16 white', 'iphone 16 plus']))
