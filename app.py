f3ceb0a8f01bf54cf910a5257f4cdafa5673683a
kolo <154599184+koloideal@users.noreply.github.com>
2024-10-12 10:14:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,17 @@
-from quart import Quart, jsonify, make_response
-from quart_httpauth import HTTPBasicAuth
+from quart import Quart, jsonify
 from get_products_data.middlewares.get_pars_data_to_best_results import pars_data_to_best_results
 import logging
 import tomllib
-from werkzeug.security import generate_password_hash, check_password_hash
 
 with open('secret_data/config.toml', 'rb') as config:
     config = tomllib.load(config)
 
-auth = HTTPBasicAuth()
 app = Quart(__name__)
 
 DEBUG = bool(int(config['Quart']['DEBUG']))
 
 
-users = {
-    "john": generate_password_hash("hello"),
-    "susan": generate_password_hash("bye")
-}
-
-
-@auth.error_handler
-def unauthorized():
-    return await make_response(jsonify({'error': 'Unauthorized access'}), 401)
-
-
-@auth.verify_password
-def verify_password(username, password):
-    if username in users and \
-            check_password_hash(users.get(username), password):
-        return username
-
-
 @app.route('/api/v1.0/search/q=<query>', methods=['GET'])
-@auth.login_required
 async def main_view(query):
     query = query.replace('+', ' ')
     data = pars_data_to_best_results.pars_data_to_best_result(query)
