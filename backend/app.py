1c2cf5441886b79a07c652db799abc42a95ee8a9
koloideal <sabatt148@gmail.com>
2024-10-24 13:04:01 +0300

    some refactor error logic

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,14 @@
 from typing import Dict
-from quart import Quart, jsonify, render_template
+from quart import Quart, jsonify, render_template, request, abort
 from quart.wrappers import Response
 from .get_products_data.collecting_primary_data.product_models import MarketPlaceList
 from .get_products_data.output_of_results import output_of_results
 import logging
 from .utils.get_config.get_quart_config import GetQuartConfig
-from .middleware.reject_middlware import RejectMiddleware
+from .middleware.reject_middlware import reject_middlware
 
 
 app: Quart = Quart(__name__)
-app.asgi_app = RejectMiddleware(app.asgi_app, "/api/v1.0/search/")
-
 config: Dict = GetQuartConfig.quart_settings()
 
 DEBUG: bool = bool(int(config["DEBUG"]))
@@ -18,19 +16,28 @@ HOST: str = config["HOST"]
 PORT: int = int(config["PORT"])
 
 
-@app.route("/unauthorized")
-async def unauthorized_view():
+@app.errorhandler(404)
+async def notfound_view(error) -> str:
+    return await render_template("notfound_page.html")
+
+
+@app.errorhandler(403)
+async def unauthorized_view(error) -> str:
     return await render_template("unauthorized_page.html")
 
 
-@app.route("/api/v1.0/search/q=<query>", methods=["GET"])
-async def main_view(query) -> Response:
-    query: str = query.replace("+", " ")
-    data: MarketPlaceList = await output_of_results(query)
+@app.route("/api/search&q=<query>", methods=["GET"])
+async def main_view(query) -> Response | str:
+    is_allowed: bool = await reject_middlware(request)
+    if is_allowed:
+        query: str = query.replace("+", " ")
+        data: MarketPlaceList = await output_of_results(query)
 
-    json_data: Dict = data.get_json()
+        json_data: Dict = data.get_json()
 
-    return jsonify({"data": json_data})
+        return jsonify({"data": json_data})
+    else:
+        await abort(403)
 
 
 def backend_run():
@@ -41,8 +48,7 @@ def backend_run():
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s\n\n\n",
     )
 
-    logging.warning("Start Product Analyzer...")
-
-    print("\n\033[1m\033[30m\033[44m {} \033[0m".format("Starting Product Analyzer..."))
+    logging.warning("Start Findly...")
+    print("\n\033[1m\033[30m\033[44m {} \033[0m".format("Start Findly..."))
 
     app.run(debug=DEBUG, host=HOST, port=PORT)
--- backend/middleware/reject_middlware.py
+++ backend/middleware/reject_middlware.py
@@ -1,34 +1,13 @@
 from typing import Dict, List
 from backend.utils.get_config.get_quart_config import GetQuartConfig
+from quart import Request
 
 
-class RejectMiddleware:
-    def __init__(self, app, path_to_protect):
-        self.app = app
-        self.path_to_protect = path_to_protect
+async def reject_middlware(request: Request):
+    quart_security: Dict = GetQuartConfig.quart_security()
+    allowed_ips: List = list(quart_security["allowed_ips"])
 
-    async def __call__(self, scope, receive, send):
-        if scope["path"].startswith(self.path_to_protect):
-            quart_security: Dict = GetQuartConfig.quart_security()
-            allowed_ips: List = list(quart_security["allowed_ips"])
-
-            if scope["client"][0] in allowed_ips:
-                return await self.app(scope, receive, send)
-            else:
-                return await self.redirect_response(send, "/unauthorized")
-        else:
-            return await self.app(scope, receive, send)
-
-    @staticmethod
-    async def redirect_response(send, location):
-        await send(
-            {
-                "type": "http.response.start",
-                "status": 301,
-                "headers": [
-                    (b"location", location.encode("utf-8")),
-                    (b"content-type", b"text/html; charset=utf-8"),
-                ],
-            }
-        )
-        await send({"type": "http.response.body", "body": b"Redirecting..."})
+    if request.remote_addr in allowed_ips:
+        return True
+    else:
+        return False
