4cbc8a48ac12defcec0bdb4b07560785ad332d08
Inlinera <ilkvakovv@gmail.com>
2024-11-09 12:49:15 +0500

    frontend: fix: layout

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -2,12 +2,10 @@ import { observer } from 'mobx-react-lite'
 import s from './index.module.scss'
 //COMPONENTS
 import { CardsWidget } from '@/widgets/card-list'
-import { SearchInputUI } from '@/shared/ui/input'
 
 export const HomePage = observer(() => {
   return (
     <div className={`${s.home_page} df fdc jcc aic`}>
-      <SearchInputUI />
       <div>
         <CardsWidget />
       </div>
